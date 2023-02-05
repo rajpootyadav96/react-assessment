2a9d39ded5d087dcd27757bc81311bc251b87cb1
rajpootyadav96 <rajpootyadav96@gmail.com>
2023-02-06 00:52:03 +0530

    userList UI updated

--- src/Screens/UserList.js
+++ src/Screens/UserList.js
@@ -11,7 +11,7 @@ const UserList = () => {
     const [headers, setheaders] = useState()
     const [isFilter, setIsFilter] = useState(false)
     const [filteredData, setFilteredResults] = useState()
-    const [showList, setShowList] = useState(false)
+    const [showList, setShowList] = useState(true)
 
 
     // get Data from api
@@ -67,9 +67,15 @@ const UserList = () => {
 
     return (
         <div>
-            <div className='pt-5 pl-2 font-xl'>Select Type
+            <div className='pt-5 pl-2 font-xl bg-teal-700 color-white flex justify-between'>
+                <div className='font-bold text-xl text-white pb-5'>
+                
                 <Navigation clickList={() => toggleData('list')}   // navigation component
                     clickCard={() => toggleData('cardList')} />
+                </div>
+                    <div>
+                            <input type='search' onChange={(e) => handleFilterChange(e.target.value)} className='border-[#243c5a] border textarea rounded pr-2 pt-1 mr-5 pb-1 pl-2' placeholder='Search...' />
+                        </div>
             </div>
 
             {showList ?
@@ -77,12 +83,10 @@ const UserList = () => {
                     <div className='h-20 flex justify-between pr-10 pt-8 pl-2 pr-2'>
                         <div className='text-2xl font-bold'>User List</div>
                         {isFilter && <div className='p-2 bg-sky-500/50 border rounded'>You are viewing filtered results!</div>}
-                        <div>
-                            <input type='search' onChange={(e) => handleFilterChange(e.target.value)} className='border-[#243c5a] border textarea rounded pr-2 pt-1 mr-5 pb-1 pl-2' placeholder='Search...' />
-                        </div>
+                        
                     </div>
                     <div >
-                        <table className='w-screen table-auto'>
+                        <table className='w-screen table-fixed'>
                             <thead>
                                 <tr className='bg-slate-200'>
                                     {headers?.map((item, idx) => {
@@ -108,17 +112,21 @@ const UserList = () => {
                         </table>
                     </div>
                 </div>
-                : <div> 
-                    <div className='text-2xl font-bold'>Card List</div>
+                : <div className='pl-5'> 
+                    <div className='text-2xl pt-8 font-bold mb-5'>Card List</div>
+                    <div className="grid grid-cols-3">
                     {
                         filteredData?.map((item, idx) => {
 
-                            return (<div className='h-3/5 w-2/6 p-5'>
+                            return (<div 
+                            // className='h-3/5 w-2/6 p-5 flex-row'
+                            >
                                 <Card data={item} /> 
                             </div>
                             )
                         })
                     }
+                    </div>
                 </div>}
 
         </div>
